1f5f7da8d73e3803b12452195862b14dce9418dc
Dennis Labordus <dennis.labordus@alliander.com>
2021-06-24 12:02:32 +0200

    Small changes to make live easier.
    Signed-off-by: Dennis Labordus <dennis.labordus@alliander.com>

--- core-commons/src/test/java/org/lfenergy/compas/commons/CompasExtensionsManagerTest.java
+++ core-commons/src/test/java/org/lfenergy/compas/commons/CompasExtensionsManagerTest.java
@@ -1,18 +1,12 @@
 package org.lfenergy.compas.commons;
 
 import org.junit.jupiter.api.Test;
-import org.lfenergy.compas.scl.SCL;
-import org.lfenergy.compas.scl.TPrivate;
 import org.lfenergy.compas.scl.extensions.TSclFileType;
-import org.lfenergy.compas.scl.extensions.common.CompasExtensionsField;
-
-import javax.xml.bind.JAXBElement;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.lfenergy.compas.scl.extensions.common.CompasExtensionsConstants.COMPAS_SCL_EXTENSION_TYPE;
-import static org.lfenergy.compas.scl.extensions.common.CompasExtensionsField.SCL_NAME_EXTENSION;
 import static org.lfenergy.compas.scl.extensions.common.CompasExtensionsField.SCL_FILETYPE_EXTENSION;
+import static org.lfenergy.compas.scl.extensions.common.CompasExtensionsField.SCL_NAME_EXTENSION;
 import static org.lfenergy.compas.util.ReadTestFile.readSCL;
 
 class CompasExtensionsManagerTest {
@@ -28,6 +22,13 @@ class CompasExtensionsManagerTest {
     }
 
     @Test
+    void getCompasPrivate_WhenCalledNullPassed_ThenNoCompasPrivateReturned() {
+        var compasPrivate = manager.getCompasPrivate(null);
+
+        assertFalse(compasPrivate.isPresent());
+    }
+
+    @Test
     void getCompasPrivate_WhenCalledWithoutPrivate_ThenNoCompasPrivateReturned() throws Exception {
         var scl = readSCL("scl_without_compas_private.scd");
 
@@ -37,7 +38,24 @@ class CompasExtensionsManagerTest {
     }
 
     @Test
-    void getCompasElement_WhenCalledWithFilename_ThenFilenameReturned() throws Exception {
+    void getCompasElement_WhenCalledNullPassed_ThenNoNameReturned() {
+        var compaselement = manager.getCompasElement(null, SCL_NAME_EXTENSION);
+
+        assertFalse(compaselement.isPresent());
+    }
+
+    @Test
+    void getCompasElement_WhenCalledWithoutName_ThenNoNameReturned() throws Exception {
+        var scl = readSCL("scl_without_name_compas_private.scd");
+
+        var compasPrivate = manager.getCompasPrivate(scl);
+        var compaselement = manager.getCompasElement(compasPrivate.get(), SCL_NAME_EXTENSION);
+
+        assertFalse(compaselement.isPresent());
+    }
+
+    @Test
+    void getCompasElement_WhenCalledWithName_ThenNameReturned() throws Exception {
         var scl = readSCL("scl_with_compas_private.scd");
 
         var compasPrivate = manager.getCompasPrivate(scl);
@@ -45,6 +63,16 @@ class CompasExtensionsManagerTest {
 
         assertTrue(compaselement.isPresent());
         assertEquals("project", compaselement.get().getValue().toString());
+    }
+
+    @Test
+    void getCompasElement_WhenCalledWithoutType_ThenNoNameReturned() throws Exception {
+        var scl = readSCL("scl_without_type_compas_private.scd");
+
+        var compasPrivate = manager.getCompasPrivate(scl);
+        var compaselement = manager.getCompasElement(compasPrivate.get(), SCL_FILETYPE_EXTENSION);
+
+        assertFalse(compaselement.isPresent());
     }
 
     @Test
