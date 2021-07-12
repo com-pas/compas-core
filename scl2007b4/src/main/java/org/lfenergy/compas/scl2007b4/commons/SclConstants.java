d45b552e4594ae80b88192588ff70996eb282883
Dennis Labordus <dennis.labordus@alliander.com>
2021-07-12 14:46:38 +0200

    Removed XSD's and used them from compas-scl-xsd project. Renamed module to scl2007b4.
    Signed-off-by: Dennis Labordus <dennis.labordus@alliander.com>

---
+++ scl2007b4/src/main/java/org/lfenergy/compas/scl2007b4/commons/SclConstants.java
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: 2020 RTE FRANCE
+//
+// SPDX-License-Identifier: Apache-2.0
+package org.lfenergy.compas.scl2007b4.commons;
+
+public class SclConstants {
+    SclConstants() {
+        throw new UnsupportedOperationException("CommonConstants class");
+    }
+
+    public static final String XML_DEFAULT_NS_PREFIX = "scl";
+    public static final String SCL_NS_URI = "http://www.iec.ch/61850/2003/SCL";
+    public static final String XML_DEFAULT_XSD_PATH = "classpath:xsd/SCL2007B4/SCL.xsd";
+    public static final String JAXB_CONTEXT_PATH = "org.lfenergy.compas.scl.extensions.model";
+}
---
+++ scl2007b4/src/test/java/org/lfenergy/compas/scl2007b4/commons/MarshallerWrapperTest.java
@@ -0,0 +1,86 @@
+// SPDX-FileCopyrightText: 2020 RTE FRANCE
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.lfenergy.compas.scl2007b4.commons;
+
+import org.junit.jupiter.api.Test;
+import org.lfenergy.compas.scl2007b4.model.SCL;
+import org.lfenergy.compas.scl2007b4.model.THeader;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class MarshallerWrapperTest {
+    @Test
+    void testShouldReturnOKWhenMarshallAndUnmarshall() throws Exception {
+        MarshallerWrapper marshallerWrapper = createWrapper("classpath:application.yml");
+        SCL scl = createSCD(UUID.randomUUID(), "1.0", "1.0");
+        String xml = marshallerWrapper.marshall(scl);
+
+        scl = marshallerWrapper.unmarshall(xml.getBytes());
+        assertEquals("2007", scl.getVersion());
+        assertEquals("B", scl.getRevision());
+        assertEquals(4, scl.getRelease());
+    }
+
+    @Test
+    void testShouldReturnOKWhenMarshallAndUnmarshallWithMapInitialization() throws Exception {
+
+        MarshallerWrapper marshallerWrapper = createWrapper(Collections.singletonMap(SclConstants.XML_DEFAULT_NS_PREFIX, SclConstants.XML_DEFAULT_XSD_PATH));
+        SCL scl = createSCD(UUID.randomUUID(), "1.0", "1.0");
+        String xml = marshallerWrapper.marshall(scl);
+
+        scl = marshallerWrapper.unmarshall(xml.getBytes());
+        assertEquals("2007", scl.getVersion());
+        assertEquals("B", scl.getRevision());
+        assertEquals(4, scl.getRelease());
+    }
+
+    @Test
+    void testShouldReturnNOKWhenMarshallCauseWrongSCLVersion() throws Exception {
+        MarshallerWrapper marshallerWrapper = createWrapper("classpath:application.properties");
+        SCL scl = createSCD(UUID.randomUUID(), "1.0", "1.0");
+        scl.setVersion("2000");
+
+        assertThrows(Exception.class, () -> marshallerWrapper.marshall(scl));
+    }
+
+    @Test
+    void testShouldReturnNOKWhenUnmarshallCauseWrongSCLVersion() throws Exception {
+        final String PAYLOAD =
+                "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
+                        "<SCL version=\"2000\" revision=\"B\" release=\"4\" xmlns=\"http://www.iec.ch/61850/2003/SCL\">\n" +
+                        "    <Header id=\"c44577af-1827-4e8c-9240-735a0ec47738\" version=\"1.0\" revision=\"1.0\"/>\n" +
+                        "</SCL>";
+
+        MarshallerWrapper marshallerWrapper = createWrapper("");
+        assertThrows(Exception.class, () -> marshallerWrapper.unmarshall(PAYLOAD.getBytes()));
+    }
+
+    private MarshallerWrapper createWrapper(String path) throws Exception {
+        return (new MarshallerWrapper.Builder()).withProperties(path).build();
+    }
+
+    private MarshallerWrapper createWrapper(Map<String,String> schemaMap) throws Exception {
+        return (new MarshallerWrapper.Builder()).withSchemaMap(schemaMap).build();
+    }
+
+    private SCL createSCD(UUID uuid, String hRevision, String hVersion){
+        SCL scl = new SCL();
+        scl.setVersion("2007");
+        scl.setRevision("B");
+        scl.setRelease((short) 4);
+        THeader tHeader = new THeader();
+        tHeader.setRevision(hRevision);
+        tHeader.setVersion(hVersion);
+        tHeader.setId(uuid.toString());
+        scl.setHeader(tHeader);
+
+        return scl;
+    }
+}
