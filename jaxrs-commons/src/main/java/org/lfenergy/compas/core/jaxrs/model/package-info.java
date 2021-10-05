e4f66fe593db80b6936036856ec8ade521ed3724
Dennis Labordus <dennis.labordus@alliander.com>
2021-10-05 07:49:27 +0200

    Rename module to jaxrs-common.
    Signed-off-by: Dennis Labordus <dennis.labordus@alliander.com>

--- jaxrs-commons/src/main/java/org/lfenergy/compas/core/jaxrs/model/package-info.java
+++ jaxrs-commons/src/main/java/org/lfenergy/compas/core/jaxrs/model/package-info.java
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2021 Alliander N.V.
 //
 // SPDX-License-Identifier: Apache-2.0
-@XmlSchema(xmlns = {@XmlNs(prefix = "jaxrs-commons", namespaceURI = COMPAS_COMMONS_V1_NS_URI)})
+@XmlSchema(xmlns = {@XmlNs(prefix = "compas-commons", namespaceURI = COMPAS_COMMONS_V1_NS_URI)})
 package org.lfenergy.compas.core.jaxrs.model;
 
 import javax.xml.bind.annotation.XmlNs;
