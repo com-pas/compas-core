44fe7ca7dfa8e3018a84aea796c99e8fe0aa75b8
Dennis Labordus <dennis.labordus@alliander.com>
2022-11-08 07:48:27 +0100

    Refactoring Encoder/Decoder.
    Signed-off-by: Dennis Labordus <dennis.labordus@alliander.com>

--- websocket-commons/src/main/java/org/lfenergy/compas/core/websocket/ErrorResponseEncoder.java
+++ websocket-commons/src/main/java/org/lfenergy/compas/core/websocket/ErrorResponseEncoder.java
@@ -3,28 +3,10 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.lfenergy.compas.core.websocket;
 
-import org.lfenergy.compas.core.commons.exception.CompasException;
 import org.lfenergy.compas.core.commons.model.ErrorResponse;
 
-import javax.xml.bind.JAXBContext;
-import java.io.StringWriter;
-
-import static org.lfenergy.compas.core.commons.exception.CompasErrorCode.WEBSOCKET_ENCODER_ERROR_CODE;
-
-public class ErrorResponseEncoder extends AbstractEncoder<ErrorResponse> {
-    @Override
-    public String encode(ErrorResponse response) {
-        try {
-            var jaxbContext = JAXBContext.newInstance(ErrorResponse.class);
-            var marshaller = jaxbContext.createMarshaller();
-
-            var st = new StringWriter();
-            marshaller.marshal(response, st);
-            return st.toString();
-        } catch (Exception exp) {
-            throw new CompasException(WEBSOCKET_ENCODER_ERROR_CODE,
-                    "Error marshalling Error Response to Websockets.",
-                    exp);
-        }
+public class ErrorResponseEncoder extends AbstractJaxbEncoder<ErrorResponse> {
+    public ErrorResponseEncoder() {
+        super(ErrorResponse.class);
     }
 }
